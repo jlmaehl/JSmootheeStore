0bf1572bbc46573904554fafb45e460f6894b675
Jamie <jamiemaehl@gmail.com>
2022-04-30 08:59:13 -0500

    angular project setup

---
+++ JSmootheesStore/android/app/src/main/java/com/jsmoothee/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jsmoothee.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ JSmootheesStore/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jsmoothee.app',
+  appName: 'jsmoothees-store',
+  webDir: 'dist/angularCapacitor',
+  bundledWebRuntime: false
+};
+
+export default config;
